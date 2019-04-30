84418f07b40cbccc2b0b5f1d723ee8f3f6f8b777
Motoki Kimura <motoki.kimura.1990@gmail.com>
2019-04-30 21:55:12 +0900

    Update octconv.py

--- octconv.py
+++ octconv.py
@@ -13,18 +13,12 @@ class OctConv2d(nn.Module):
     Drop an Octave: Reducing Spatial Redundancy in Convolutional Neural Networks with Octave Convolution.
     paper link: https://arxiv.org/abs/1904.05049
     """
-    def __init__(self, in_channels, out_channels, kernel_size, stride=1, alphas=(0.5, 0.5)):
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1, bias=True, alphas=(0.5, 0.5)):
         super(OctConv2d, self).__init__()
-
-        if stride != 1:
-            raise NotImplementedError()
-            # FIXME: correctly implement strided OctConv.
-            # references:
-            # https://github.com/iacolippo/octconv-pytorch/issues/3
-            # https://github.com/terrychenism/OctaveConv/issues/4
 
         alpha_in, alpha_out = alphas
         assert (0 <= alpha_in <= 1) and (0 <= alpha_in <= 1), "Alphas must be in interval [0, 1]"
+
         # input channels
         self.ch_in_lf = int(alpha_in * in_channels)
         self.ch_in_hf = in_channels - self.ch_in_lf
@@ -32,19 +26,19 @@ class OctConv2d(nn.Module):
         self.ch_out_lf = int(alpha_out * out_channels)
         self.ch_out_hf = out_channels - self.ch_out_lf
 
-        # filters
-        self.wHtoH, self.wHtoL, self.wLtoH, self.wLtoL = None, None, None, None
-        if not (self.ch_out_hf == 0 or self.ch_in_hf == 0):
-            self.wHtoH = nn.Parameter(torch.randn(self.ch_out_hf, self.ch_in_hf, kernel_size, kernel_size))
-        if not (self.ch_out_lf == 0 or self.ch_in_hf == 0):
-            self.wHtoL = nn.Parameter(torch.randn(self.ch_out_lf, self.ch_in_hf, kernel_size, kernel_size))
-        if not (self.ch_out_hf == 0 or self.ch_in_lf == 0):
-            self.wLtoH = nn.Parameter(torch.randn(self.ch_out_hf, self.ch_in_lf, kernel_size, kernel_size))
-        if not (self.ch_out_lf == 0 or self.ch_in_lf == 0):
-            self.wLtoL = nn.Parameter(torch.randn(self.ch_out_lf, self.ch_in_lf, kernel_size, kernel_size))
-
         # padding: (H - F + 2P)/S + 1 = 2 * [(0.5 H - F + 2P)/S +1] -> P = (F-S)/2
-        self.padding = (kernel_size - stride) // 2
+        padding = (kernel_size - stride) // 2
+
+        # conv layers
+        self.HtoH, self.HtoL, self.LtoH, self.LtoL = None, None, None, None
+        if not (self.ch_out_hf == 0 or self.ch_in_hf == 0):
+            self.HtoH = nn.Conv2d(self.ch_in_hf, self.ch_out_hf, kernel_size, stride, padding, bias=bias)
+        if not (self.ch_out_lf == 0 or self.ch_in_hf == 0):
+            self.HtoL = nn.Conv2d(self.ch_in_hf, self.ch_out_lf, kernel_size, stride, padding, bias=bias)
+        if not (self.ch_out_hf == 0 or self.ch_in_lf == 0):
+            self.LtoH = nn.Conv2d(self.ch_in_lf, self.ch_out_hf, kernel_size, stride, padding, bias=bias)
+        if not (self.ch_out_lf == 0 or self.ch_in_lf == 0):
+            self.LtoL = nn.Conv2d(self.ch_in_lf, self.ch_out_lf, kernel_size, stride, padding, bias=bias)
 
     def forward(self, x):
         hf, lf = None, None
@@ -58,22 +52,19 @@ class OctConv2d(nn.Module):
             hf, lf = x
 
         # apply convolutions
-        HtoH = HtoL = LtoL = LtoH = 0.
-        if self.wHtoH is not None:
-            HtoH = F.conv2d(hf, self.wHtoH, padding=self.padding)
-        if self.wHtoL is not None:
-            HtoL = F.conv2d(F.avg_pool2d(hf, 2), self.wHtoL, padding=self.padding)
-        if self.wLtoH is not None:
-            LtoH = F.interpolate(
-                F.conv2d(lf, self.wLtoH, padding=self.padding),
-                scale_factor=2, mode='nearest'
-            )
-        if self.wLtoL is not None:
-            LtoL = F.conv2d(lf, self.wLtoL, padding=self.padding)
+        oHtoH = oHtoL = oLtoH = oLtoL = 0.
+        if self.HtoH is not None:
+            oHtoH = self.HtoH(hf)
+        if self.HtoL is not None:
+            oHtoL = self.HtoL(F.avg_pool2d(hf, 2))
+        if self.LtoH is not None:
+            oLtoH = F.interpolate(self.LtoH(lf), scale_factor=2, mode='nearest')
+        if self.LtoL is not None:
+            oLtoL = self.LtoL(lf)
 
         # compute output tensors
-        hf = HtoH + LtoH
-        lf = LtoL + HtoL
+        hf = oHtoH + oLtoH
+        lf = oLtoL + oHtoL
 
         # logic to handle output tensors:
         # if ch_out_lf = 0., we assume to be at the last layer, with only high freq repr
